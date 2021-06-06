6d6ba9ef6cc33c96a394fd6bda874f628675f25a
rarawe2000 <carina.volland@web.de>
2021-06-06 23:48:10 +0200

    04-5

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -6,6 +6,8 @@ var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
