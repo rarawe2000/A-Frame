574c5d765a22b133f9f22d82faf3a623bde14d3d
rarawe2000 <carina.volland@web.de>
2021-06-24 00:19:39 +0200

    04-7 finalized

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,37 +1,3 @@
-
-
-// changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-
-// var myOtherBox = document.getElementById('myOtherBox');
-
-// function spin(){
-// 	var axis = [x,y,z];
-
-// 	for (i = 0; i < axis.length; i++) { 
-//   		myOtherBox.object3D.rotation.axis[i] -= rotationSpeed;
-// 	}
-
-// 	// myOtherBox.object3D.rotation.x -= rotationSpeed;
-// 	// myOtherBox.object3D.rotation.y -= rotationSpeed;
-// 	// myOtherBox.object3D.rotation.z -= rotationSpeed;
-
-// 	rotationSpeed += 0.49;
-
-// 	for (i = 0; i < axis.length; i++) { 
-//   		myOtherBox.object3D.rotation.axis[i] -= rotationSpeed;
-// 	}
-
-// 	rotationSpeed -= 0.49;
-
-
-// 	// console.log(myOtherBox.object3D.rotation.x);
-// }
-
-// setInterval(spin, 16); //equivalent to 60 fps
-
-//-------------------------------------
-
 // changing individual properties with code and using setInterval
 var rotationSpeed = 0.01;
 var widthIncrease = 0.1;
@@ -82,10 +48,6 @@ function spin(){
 
 	if (myOtherBox.object3D.position.y >= -3 && myOtherBox.object3D.position.y <= 10) {
 		positionChangeY = 0.01;
-	} 
-
-	if (myOtherBox.object3D.position.y == 9.99) {
-		positionChangeY -= 9.99;
 	}
 
 	console.log(myOtherBox.object3D.rotation.x);
